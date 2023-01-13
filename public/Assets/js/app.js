0d71860671ca624858757f5b745bec52e723af09
Yasith Wimukthi <yasith@imperiallearning.co.uk>
2023-01-13 14:59:31 +0530

    implement audio stream

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,15 +1,23 @@
 let MyApp = (function (){
+    let socket = null;
+    let meetingId = null;
+    let userId = null;
+
     function init(uid,mid){
-        console.log('MyApp.init()');
+        userId = uid;
+        meetingId = mid;
         eventProcessForSignalingServer();
     }
 
-    let socket = null;
 
     const eventProcessForSignalingServer = () => {
         socket = io.connect();
         socket.on('connect', () => {
-            console.log("socket connect to client side");
+            if (socket.connected){
+                if (meetingId && userId){
+                    socket.emit('userConnect', {meetingId: meetingId, displayName: userId});
+                }
+            }
         });
     }
 
